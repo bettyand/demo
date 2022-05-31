24ddc87ee03829c5a1128cad3c9c488058d83b4c
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-05-31 09:27:21 -0400

    init

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Greeting {
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
